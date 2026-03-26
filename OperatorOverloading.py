e01866693c0c8d841642055c060c9519fe0ccd00
Kairaa2014 <kairaasharma22@gmail.com>
2026-03-26 09:01:46 +0530

    demo

---
+++ OperatorOverloading.py
@@ -0,0 +1,30 @@
+class a:
+    def __init__(self,a):
+        self.a = a
+    def __lt__(self, other):
+        if(self.a>other.a):
+            return"ob1 is less than ob2"
+        else:
+            return"ob2 is less than ob1"
+        
+    def __eq__(self,other):
+        if(self.a == other.a):
+            return"Both are equal."
+        else:
+            return"Both are not equal"
+        
+ob1 = a(2)
+ob2 = a(3)
+
+print("Passed values, ",ob1.a ,ob2.a)
+print(ob1 < ob2)
+
+ob3 = a(4)
+ob4 = a(5)
+
+print("Passed values, ",ob3.a, ob4.a)
+print(ob3 == ob4)
+
+        
+
+
